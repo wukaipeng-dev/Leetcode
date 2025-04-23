245d0fdc10fc721583e53339c0d59079601efd24
楷鹏 : ) <wkpcoder@163.com>
2025-04-23 09:29:29 +0800

    ✨ feat: implement selection sort algorithm with complete functionality and return sorted array

--- 十大排序/1-选择排序/index.js
+++ 十大排序/1-选择排序/index.js
@@ -4,6 +4,22 @@
  * @param {Function} compareFn - 比较函数，默认按升序排序
  * @returns {Array} 排序后的数组
  */
-function selectionSort(arr, compareFn = (a, b) => a - b) {}
+function selectionSort(arr, compareFn = (a, b) => a - b) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let extremeValueIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (compareFn(arr[extremeValueIndex], arr[j]) > 0) {
+        extremeValueIndex = j;
+      }
+    }
+    if (extremeValueIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[extremeValueIndex];
+      arr[extremeValueIndex] = temp;
+    }
+  }
+
+  return arr;
+}
 
 module.exports = selectionSort;
