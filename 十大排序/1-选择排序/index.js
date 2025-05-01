2b0187a1d86d2659f7f65f9cd465a9d557ecb87b
楷鹏 : ) <wkpcoder@163.com>
2025-05-01 14:37:28 +0800

    ✨ feat: implement initial selection sort function with placeholder for code

--- 十大排序/1-选择排序/index.js
+++ 十大排序/1-选择排序/index.js
@@ -4,6 +4,8 @@
  * @param {Function} compareFn - 比较函数，默认按升序排序
  * @returns {Array} 排序后的数组
  */
-function selectionSort(arr, compareFn = (a, b) => a - b) {}
+function selectionSort(arr, compareFn = (a, b) => a - b) {
+  // Writing your code here
+}
 
 module.exports = selectionSort;
